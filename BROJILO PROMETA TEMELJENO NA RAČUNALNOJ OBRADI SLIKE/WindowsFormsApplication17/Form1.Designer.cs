f1a4d24bbcbc40fcfed8d3d2b3ceffa9e2150f5d
Filip2m <40461094+Filip2m@users.noreply.github.com>
2018-11-25 21:49:39 +0100

    traffic counter

---
+++ BROJILO PROMETA TEMELJENO NA RAČUNALNOJ OBRADI SLIKE/WindowsFormsApplication17/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace WindowsFormsApplication17
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ibVideo = new Emgu.CV.UI.ImageBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.KontrolniVideo = new Emgu.CV.UI.ImageBox();
+            this.Reset_counter = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.ispisxy = new System.Windows.Forms.Label();
+            this.Uputa = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ibVideo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.KontrolniVideo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ibVideo
+            // 
+            this.ibVideo.Location = new System.Drawing.Point(13, 13);
+            this.ibVideo.Name = "ibVideo";
+            this.ibVideo.Size = new System.Drawing.Size(640, 360);
+            this.ibVideo.TabIndex = 2;
+            this.ibVideo.TabStop = false;
+            this.ibVideo.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ibVideo_MouseClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(710, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Width";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(710, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Height";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(710, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Data";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(657, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Prolazaka_Lijevo:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(758, 131);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(7, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "\r\n";
+            // 
+            // KontrolniVideo
+            // 
+            this.KontrolniVideo.Location = new System.Drawing.Point(659, 187);
+            this.KontrolniVideo.Name = "KontrolniVideo";
+            this.KontrolniVideo.Size = new System.Drawing.Size(640, 360);
+            this.KontrolniVideo.TabIndex = 2;
+            this.KontrolniVideo.TabStop = false;
+            this.KontrolniVideo.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ibVideo_MouseClick);
+            // 
+            // Reset_counter
+            // 
+            this.Reset_counter.Location = new System.Drawing.Point(831, 29);
+            this.Reset_counter.Name = "Reset_counter";
+            this.Reset_counter.Size = new System.Drawing.Size(121, 23);
+            this.Reset_counter.TabIndex = 8;
+            this.Reset_counter.Text = "Reset Counter";
+            this.Reset_counter.UseVisualStyleBackColor = true;
+            this.Reset_counter.Click += new System.EventHandler(this.Reset_counter_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(657, 158);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(94, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Prolazaka_Desno:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(758, 158);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 10;
+            // 
+            // ispisxy
+            // 
+            this.ispisxy.AutoSize = true;
+            this.ispisxy.Location = new System.Drawing.Point(28, 410);
+            this.ispisxy.Name = "ispisxy";
+            this.ispisxy.Size = new System.Drawing.Size(56, 13);
+            this.ispisxy.TabIndex = 11;
+            this.ispisxy.Text = "X,Y,brojac";
+            // 
+            // Uputa
+            // 
+            this.Uputa.AutoSize = true;
+            this.Uputa.Location = new System.Drawing.Point(10, 575);
+            this.Uputa.Name = "Uputa";
+            this.Uputa.Size = new System.Drawing.Size(747, 13);
+            this.Uputa.TabIndex = 12;
+            this.Uputa.Text = "Za postavljanje kordinata, prvo se uzima traka gdje promet nadolazi sa lijeva na " +
+    "desno (3 tocke), zatim traka gdje promet nadolazi sa desna na lijevo (3 tocke)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1370, 749);
+            this.Controls.Add(this.Uputa);
+            this.Controls.Add(this.ispisxy);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Reset_counter);
+            this.Controls.Add(this.KontrolniVideo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ibVideo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ibVideo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.KontrolniVideo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox ibVideo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private Emgu.CV.UI.ImageBox KontrolniVideo;
+        private System.Windows.Forms.Button Reset_counter;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label ispisxy;
+        private System.Windows.Forms.Label Uputa;
+    }
+}
+
---
+++ BROJILO PROMETA TEMELJENO NA RAČUNALNOJ OBRADI SLIKE/WindowsFormsApplication17/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+
+using Emgu.CV;
+using Emgu.CV.UI;
+using Emgu.Util;
+using Emgu.CV.Structure;
+
+namespace WindowsFormsApplication17
+{
+    public partial class Form1 : Form
+    {
+        private Capture _capture2 = null;
+        private Image<Bgr, Byte> colImage2;
+        private Image<Gray, byte> GrayImage;
+        private Image<Gray, Byte> oldImage2;
+        //private Image<Bgr, Byte> oldImage;
+        private Image<Gray, Byte> resultImage2;
+        private int brojac = 0;
+        private float BrL = 0;
+        private float BrR = 0;
+        private int flag1 = 0, flag2 = 0,flag3=0;
+        private int flag4 = 0, flag5 = 0, flag6 = 0;
+        private int X;
+        private int Y;
+        public int middle_oneX = 0;
+        public int left_oneX = 0;
+        public int right_oneX = 0;
+        public int middle_twoX = 0;
+        public int left_twoX = 0;
+        public int right_twoX = 0;
+
+        public int middle_oneY = 0;
+        public int left_oneY = 0;
+        public int right_oneY = 0;
+        public int middle_twoY = 0;
+        public int left_twoY = 0;
+        public int right_twoY = 0;
+        
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+            _capture2 = new Capture("parking_example_1.avi");//dohvacanje slike iz file-a
+            //_capture2 = new Capture(0); //za dohvacanje snimke sa default kamere
+            _capture2.ImageGrabbed += _capture2_ImageGrabbed;
+            _capture2.Start();
+            int W = _capture2.Width;
+            int H = _capture2.Height;
+            label1.Text = W.ToString();
+            label2.Text = H.ToString();
+            
+            
+            
+        }
+        
+        void _capture2_ImageGrabbed(object sender, EventArgs e)
+        {
+
+           
+            colImage2 = _capture2.RetrieveBgrFrame(); //kontrolni video
+            GrayImage = _capture2.RetrieveGrayFrame(); //tu se broji
+            if (oldImage2 == null) oldImage2 = GrayImage;
+           
+            resultImage2 = GrayImage - oldImage2;
+            
+
+            
+
+            resultImage2._ThresholdBinary(new Gray(20), new Gray(255));
+            resultImage2.MorphologyEx(new StructuringElementEx(10, 10, 5, 5, Emgu.CV.CvEnum.CV_ELEMENT_SHAPE.CV_SHAPE_ELLIPSE), Emgu.CV.CvEnum.CV_MORPH_OP.CV_MOP_OPEN, 20);
+            ibVideo.Image = resultImage2;                      //5,5,2,2                                                                                                //15
+
+            Point start1 = new Point(317,220);
+            Point stop1 = new Point(323, 220);                      //plava linija lijevo-desno
+            LineSegment2D line1 = new LineSegment2D(start1, stop1);
+                                                                                                //zajedno pokazuju mjesta gdje su "senzori" od A do I
+            Point start = new Point(320, 220);
+            Point stop = new Point(320, 300);                       //plava linija gore-dolje
+           
+            
+            KontrolniVideo.Image = colImage2;
+            oldImage2 = GrayImage;
+
+
+            Point startA = new Point(left_twoX, left_twoY);
+            Point stopA = new Point(right_twoX, right_twoY);                     // gornja crvena
+            LineSegment2D lin51 = new LineSegment2D(startA, stopA);
+            colImage2.Draw(lin51, new Bgr(Color.Red), 1);
+
+                byte A = resultImage2.Data[middle_twoY, middle_twoX, 0];         
+                byte AL = resultImage2.Data[left_twoY, left_twoX, 0];         // sa desna na lijevo
+                byte AR = resultImage2.Data[right_twoY, right_twoX, 0];
+                
+
+                Point startB = new Point(left_oneX, left_oneY);
+                Point stopB = new Point(right_oneX, right_oneY);                      //donja crvena sa lijeva na desno
+                LineSegment2D lin52 = new LineSegment2D(startB, stopB);
+                colImage2.Draw(lin52, new Bgr(Color.Red), 1);
+               
+                byte B = resultImage2.Data[middle_oneY, middle_oneX, 0];
+                byte BL = resultImage2.Data[left_oneY, left_oneX, 0];
+                byte BR = resultImage2.Data[right_oneY, right_oneX, 0];
+               
+
+
+
+
+                                                     
+
+                if (AR > 0 && flag1 == 0 && flag2 == 0 && flag3 == 0) flag1 = 1;
+                if (A > 0 && flag1 == 1 && flag2 == 0 && flag3 == 0) flag2 = 1;
+                if (AL > 0 && flag1 == 1 && flag2 == 1 && flag3 == 0) flag3 = 1;
+                if (flag3 == 1 && flag2==1 && flag3==1)
+                {
+                    CounterL();
+                    flag1 = 0;
+                    flag2 = 0;
+                    flag3 = 0;
+                }
+
+                if (BL > 0 && flag4==0 && flag5==0 && flag6==0) flag4 = 1;
+                if (B > 0 && flag4 == 1 && flag5 == 0 && flag6 == 0) flag5 = 1;
+                if (BR > 0 && flag4 == 1 && flag5 == 1 && flag6 == 0) flag6 = 1;
+                if (flag6 == 1 && flag4==1 && flag5==1)
+                {
+                    CounterR();
+                    flag4 = 0;
+                    flag5 = 0;
+                    flag6 = 0;
+
+                }
+
+                
+               
+
+                label5.Invoke(new Action(() =>
+                {
+                    label5.Text = BrL.ToString();
+
+                }));
+                label7.Invoke(new Action(() =>
+                {
+                    label7.Text = BrR.ToString();
+
+                }));
+
+
+               
+                
+            
+            
+
+
+
+            
+        }
+
+        private void Reset_counter_Click(object sender, EventArgs e)
+        {
+            BrL = 0;
+            BrR = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+       
+
+        private void CounterL()
+        {
+            BrL++;
+        }
+        private void CounterR()
+        {
+            BrR++;
+        }
+        private void ibVideo_MouseClick(object sender, MouseEventArgs e)
+        {
+            X = e.X;
+            Y = e.Y;
+            brojac++;
+            ispisxy.Text = X.ToString() +" "+ Y.ToString() +" "+brojac;
+            
+            switch(brojac)
+            {
+                case 1:
+                    left_oneX = X;
+                    left_oneY = Y;
+                    break;
+                case 2:
+                    middle_oneX = X;
+                    middle_oneY = Y;
+                    break;
+                case 3:
+                    right_oneX = X;
+                    right_oneY = Y;
+                    break;
+                case 4:
+                    right_twoX = X;
+                    right_twoY = Y;
+                    break;
+                case 5:
+                    middle_twoX = X;
+                    middle_twoY = Y;
+                    break;
+                case 6:
+                    left_twoX = X;
+                    left_twoY = Y;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+
+
+        
+    }
+}
